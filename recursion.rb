e3220fda7cfab50346ece7f4f485a967fcf97b3f
Enntek <abluecar@gmail.com>
2022-08-09 19:23:40 -0400

    Finish fib array method

--- recursion.rb
+++ recursion.rb
@@ -38,21 +38,61 @@ end
 
 # p iterate_fib(8)
 
-def recurse_fib(n, array = [])
+# def fib_of(n)
 
-  if n == 1
-    return 0
-  elsif n == 2
-    return 1
-  elsif n > 2
-    p array << (recurse_fib(n-1, array) + recurse_fib(n-2, array))
-    return (recurse_fib(n-1, array) + recurse_fib(n-2, array))
-  end
+#   if n == 1
+#     return 0
+#   elsif n == 2
+#     return 1
+#   elsif n > 2
+#     return (fib_of(n-1) + fib_of(n-2))
+#   end
 
+# end
+
+# def fib_array(n, array = [])
+#   if n == 1
+#     return array
+#   else
+#     array.prepend fib_of(n - 1)
+#     return fib_array(n - 1, array)
+#   end
+# end
+
+# p fib_array(8)
+
+# we can get the correct fib number given n
+# we need all of the numbers inbetween to also display somehow
+# that is, we need the entire sequence
+
+# your recursive method must have a base case
+# your recursive method must call itself with a smaller chunk of data
+
+# we have a method for finding fib of n
+# it seems like we need a helper function or similar function
+# that calls each number starting with 6, counting down
+# then shoves the result into an array
+# basically 2 functions are needed for this
+
+# p fib_of(7)
+
+# maybe the array is closer to a train
+# to get the next answer we just add the previous two numbers!!!
+# we actually solved it...
+# half luck, half deliberate thinking
+# it seems creating several if scenarios helps a lot, going through the actual sequence
+# as it's counting down, it's gathering the sequence
+# once we hit the base case, only the array is returned
+
+def fib_array(n, arr = [0, 1])
+  return [0] if n == 1
+  return arr if n == 2
+  arr << (arr[-2] + arr[-1])
+  fib_array(n - 1, arr)
 end
 
-# returns correct fib number given n
-# how do we shove the result of each method into the array?
-# what about using extra variables?
-
-p recurse_fib(4)
+# arr = [0, 1]
+# arr << (arr[-2] + arr[-1])
+# arr << (arr[-2] + arr[-1])
+# p arr
+p fib_array(8)
